d5b9b9388079043b6c0957f2179ee9e95ed04b67
vikash sharma <vikashtiwari9893@gmail.com>
2024-10-26 13:03:39 +0530

    includes method in ejs for linking head and footer section in all pages

---
+++ 16.includes_ejs/index.js
@@ -0,0 +1,12 @@
+const express =require("express");
+const app = express();
+
+app.set("view engine","ejs");
+
+app.get("/",(req,res)=>{
+    res.render("index.ejs")
+})
+
+app.listen(8000,()=>{
+   console.log("server is running")
+})
