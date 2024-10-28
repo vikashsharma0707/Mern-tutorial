1f0e4c263cb61ce4df8510ebf38d9ef2c5974cb2
vikash sharma <vikashtiwari9893@gmail.com>
2024-10-28 15:35:38 +0530

    Crud operation in ejs

--- 17.EJS_CRUD/index.js
+++ 17.EJS_CRUD/index.js
@@ -112,6 +112,22 @@ app.get("/posts/:id", (req, res) => {
     res.render("show.ejs", { post });
 });
 
+app.patch("/posts/:id",(req,res)=>{
+  let {id} = req.params;
+  let newcontent = req.body.content
+  console.log(newcontent);
+  let post = posts.find((p)=> id == p.id)
+  post.content = newcontent;
+  console.log(post)
+  res.redirect("/posts")
+})
+
+app.get("/posts/:id/edit",(req,res)=>{
+    let {id} = req.params;
+    let post = posts.find((p)=> id == p.id)
+    res.render("edit.ejs",{post})
+})
+
 app.delete("/posts/:id", (req, res) => {
     let { id } = req.params;
     posts = posts.filter((p) => id !== p.id);
